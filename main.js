f75175f160854895bc978ee03f519e5c3725d332
Zakarias Abdulkadir <z.k.abdulkadir@gmail.com>
2024-03-04 09:45:01 -0700

    post

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const wishListGame = () => {
         const result = data.filter((game) => {
             return game.title == 'Diablo Immortal'
         })
-        const img = document.querySelector('#wishlist-name')
+        const img = document.querySelector('#wishlist-image')
         const name= document.querySelector("#wishlist-name")
         const developer= document.querySelector("#wishlist-developer")
         const release = document.querySelector("#wishlist-release")
@@ -13,8 +13,6 @@ const wishListGame = () => {
         name.textContent = result[0].title
         developer.textContent = result[0].developer
         release.textContent = result[0].release_date
-        console.log(result[0])
-        console.log(developer)
     })
 }
 
@@ -23,13 +21,15 @@ const addGame = () => {
     form.addEventListener('submit', (e) => {
         e.preventDefault()
         const data = {}
-        data.name = e.target.name.value
-        data.progress = e.target.restaurant.value
-        data.comments = e.target.image.value
+        data.name = e.target["game-name"].value
+        data.progress = e.target.progress.value
+        data.comments = e.target.comments.value
         fetch('http://localhost:3000/library', {
             method: "post",
             body: JSON.stringify(data)
         })
+        .then(res => res.json())
+        .then(data => console.log(data))
   })
 }
 
